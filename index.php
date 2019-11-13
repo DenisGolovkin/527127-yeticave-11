05b0895ba604edb98f1c4a583a6b864c8bf3aa83
DenisGolovkin <den4ik.pustosloff@yandex.com>
2019-11-13 13:55:31 +0300

    Внёс правки в ф-цию форматировании цены

--- index.php
+++ index.php
@@ -57,9 +57,8 @@ function summ_round($summ) {
   if ($formated_summ >= 1000) {
       $formated_summ = number_format($formated_summ, 0, '.',' ');
   }
-  return  $formated_summ . " ₽";
+  echo  $formated_summ . '<b class="rub"> р</b>';
 }
-echo summ_round($summ);
 ?>
 
 
